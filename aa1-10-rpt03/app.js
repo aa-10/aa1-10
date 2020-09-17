cf395d7b1992e38af54143593e513459564e404d
aa1-10 <70686459+aa-10@users.noreply.github.com>
2020-09-17 17:39:41 +0900

    Add files via upload

---
+++ aa1-10-rpt03/app.js
@@ -0,0 +1,28 @@
+//app.js
+var express = require("express");
+var app = express();
+var port = 3000;
+// routing=주소창 뒤에 뭘 적는지에 따라 나오는게 달라짐
+app.get("/", function (req, res) {
+    res.send('<a href="/hello">Hello Page</a>');
+});
+
+app.get("/hello", function (req, res) {
+    res.send("Hello aa10");
+});
+
+app.get("/comsi", function (req, res) {
+    res.send("Hello Comsi!");
+});
+
+app.get("/aa10", function (req, res) {
+    res.send('Hello aa10, Comsi! My first express server!!!');
+});
+
+app.get("/naver", function (req, res) {
+    res.send('<a href="http://www.naver.com">Go to naver</a>');
+});
+
+var server = app.listen(port, function() {
+    console.log("Listening on port %d", server.address().port);
+});
